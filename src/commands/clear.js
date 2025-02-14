c693cbf7cb28e1b26cdcdc2aff96985380e8c627
Kevin Sullivan <kevin@sideapps.com>
2025-02-14 13:10:56 -0700

    Fix clear command

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -3,7 +3,14 @@ module.exports = {
     description: 'Clear the conversation history',
     async execute(message) {
         const conversationHandler = message.client.conversationHandler;
-        conversationHandler.clearHistory(message.channel.id, message.guild?.id);
+        const isDM = message.channel.type === 1;
+
+        if (isDM) {
+            conversationHandler.clearDMHistory(message.author.id);
+        } else {
+            conversationHandler.clearTextHistory(message.channel.id, message.guild?.id);
+        }
+        
         await message.reply('🧹 Conversation history cleared!');
     }
 }; 
